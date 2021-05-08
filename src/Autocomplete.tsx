51950dc7c3f67cf3e78327eccff9836060d2d378
Pravin Bashyal <pravin.bashyal@gmail.com>
2021-05-08 11:17:50 +0200

    init

---
+++ src/Autocomplete.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+export type AutocompleteProps<T> = {
+  id?: string;
+  label: string;
+  getLabel?: (option: T) => string;
+  getValue?: (option: T) => any;
+  options: T[];
+};
+
+function defaultGetter<T>(option: T) {
+  return (option as never) as string;
+}
+
+export function Autocomplete<T>({
+  id,
+  label,
+  getLabel = defaultGetter,
+  getValue = defaultGetter,
+  options,
+}: AutocompleteProps<T>) {
+  return (
+    <div className="field">
+      <label htmlFor={id}>
+        <span className="field-label">{label}</span>
+      </label>
+      <select
+        name="destination"
+        aria-hidden="true"
+        tabIndex={-1}
+        className="visually-hidden"
+      ></select>
+      <div className="autocomplete">
+        <input
+          aria-owns="autocomplete-options--destination"
+          autoCapitalize="none"
+          type="text"
+          autoComplete="off"
+          aria-autocomplete="list" // eslint-disable-next-line jsx-a11y/role-has-required-aria-props
+          role="combobox"
+          id="destination"
+          aria-expanded="false"
+        />
+        <svg
+          focusable="false"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+        ></svg>
+        <ul
+          id="autocomplete-options--destination"
+          role="listbox"
+          className="hidden"
+        >
+          {options.map((option, index) => (
+            <li
+              role="option"
+              tabIndex={-1}
+              aria-selected="false"
+              data-option-value="1"
+              id={`autocomplete_${index}`}
+            >
+              {getLabel(option)}
+              {getValue(option)}
+            </li>
+          ))}
+        </ul>
+        <div aria-live="polite" role="status" className="visually-hidden">
+          13 results available.
+        </div>
+      </div>
+    </div>
+  );
+}
