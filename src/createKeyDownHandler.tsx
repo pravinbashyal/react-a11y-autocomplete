5f5cd5935369b107f12dcefb4e4d002916af5c71
Pravin Bashyal <pravin.bashyal@gmail.com>
2021-05-10 08:41:36 +0200

    rename file
    renamed:    src/createKeyUpHandler.tsx -> src/createKeyDownHandler.tsx

--- src/createKeyDownHandler.tsx
+++ src/createKeyDownHandler.tsx
@@ -1,37 +1,27 @@
 import keycode from "keycode";
 import { EssentialKeys } from "./EssentialKeys";
 
-export const createKeyUpHandler = ({
+export const createKeyDownHandler = ({
   showOptions,
-  hideOptions,
   navigateUp,
   navigateDown,
-  navigateLeft,
-  navigateRight,
   onEnterPress,
+  hideOptions,
 }: {
   showOptions: () => void;
   hideOptions: () => void;
   navigateUp: () => void;
   navigateDown: () => void;
-  navigateLeft: () => void;
-  navigateRight: () => void;
+  onEnterPress: () => void;
 }) => (e) => {
-  console.log("focused");
   const pressedKey = keycode(e);
-  console.log(keycode(e));
+  console.log(pressedKey);
   switch (pressedKey) {
     case EssentialKeys.Down:
       navigateDown();
       break;
     case EssentialKeys.Up:
       navigateUp();
-      break;
-    case EssentialKeys.Left:
-      navigateLeft();
-      break;
-    case EssentialKeys.Right:
-      navigateRight();
       break;
     case EssentialKeys.Enter:
       onEnterPress();
