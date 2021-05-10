8d8dd147473896fce5e5303ab7222e0527784010
Pravin Bashyal <pravin.bashyal@gmail.com>
2021-05-10 08:42:33 +0200

    add fuzzy search

---
+++ src/useFilteredOptions.tsx
@@ -0,0 +1,37 @@
+import Fuse from "fuse.js";
+import { useEffect, useMemo, useState } from "react";
+
+const config = {
+  includeMatches: true,
+  minMatchCharLength: 0,
+};
+
+type FilteredOption = Partial<Fuse.FuseResult<string>>;
+
+export function useFilteredOptions(
+  options: string[],
+  textInputIntermediateValue: string,
+  maxOptions: number
+): Array<FilteredOption> {
+  const defaultOptions = useMemo(
+    () => options.map((option) => ({ item: option })),
+    [options]
+  );
+  const [filteredOptions, setFilteredOptions] = useState<Array<FilteredOption>>(
+    options.map((option) => ({ item: option }))
+  );
+
+  const fuse = useMemo(() => {
+    return new Fuse(options, config);
+  }, [options]);
+
+  useEffect(() => {
+    const filtered = fuse.search(textInputIntermediateValue);
+    if (filtered.length === 0) {
+      setFilteredOptions(defaultOptions);
+      return;
+    }
+    setFilteredOptions(filtered);
+  }, [defaultOptions, fuse, maxOptions, options, textInputIntermediateValue]);
+  return filteredOptions;
+}
